6d146767fba666f7c66841970e2ac6ea1090d7fd
Sayyed <Wasim.Sayyed@emc.com>
2020-07-03 08:46:47 +0530

    added spinner

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 function SearchList(props) {
 
     const { flight } = props;
---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spinner = () => {
+	return(
+		<section className="splash_outer">
+			<div className="splash_inner">
+				<div className="spinner_img"></div>
+			</div>
+		</section>
+	)	
+}
+
+export default Spinner;
